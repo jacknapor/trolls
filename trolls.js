34b77667020f324f71af619c81a54bcb25fae918
Jack Napor <jack@napor.dev>
2023-08-29 18:16:19 -0400

    more xss ??

--- trolls.js
+++ trolls.js
@@ -1,4 +1,5 @@
 $(function(){
+    document.title = "Troll City";
     $(".wedding-img").click(function(){
         window.location.href = "https://www.zola.com/wedding/gjystinaandgeorge/";
     });
